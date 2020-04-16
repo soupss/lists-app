1e24d5c1cb873527ae01faa223759adcf412646a
Anton Laurell <antonlaurell@hotmail.com>
2020-04-17 01:50:38 +0200

    add bootstrap and mock data

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,7 +1,29 @@
+from datetime import datetime
 from flask import render_template
 from flaskr import app
+
+items = [
+    {
+        'title': 'Clean the house',
+        'desc': 'Bla bla bla bla bla',
+        'id': 'demo',
+        'date_posted': datetime.utcnow(),
+    },
+    {
+        'title': 'Walk the dog',
+        'desc': 'Bla bla bla bla bla',
+        'id': 'demo2',
+        'date_posted': datetime.utcnow(),
+    },
+    {
+        'title': 'Make an app',
+        'desc': 'Bla bla bla bla bla',
+        'id': 'demo3',
+        'date_posted': datetime.utcnow(),
+    }
+]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', items=items)
