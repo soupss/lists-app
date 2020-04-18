b5daf13da5ab399a533edbe5cceadcc8329f19f4
Anton Laurell <antonlaurell@hotmail.com>
2020-04-18 16:04:14 +0200

    organize routes

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,14 +1,24 @@
-from flask import render_template, request
+from flask import render_template, redirect, url_for, request
 from flaskr import app, db
-from flaskr.models import List, Item
+from flaskr.models import List
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        list_name = request.form['list']
-        list = List(name=list_name)
-        db.session.add(list)
-        db.session.commit()
+@app.route('/create-list', methods=['POST'])
+def create_list():
+    list_name = request.form['list']
+    list = List(name=list_name)
+    db.session.add(list)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/')
+def re():
+    return redirect(url_for('home'))
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    # TODO: check if there are lists
     lists = List.query.all()
     return render_template('home.html', lists=lists)
